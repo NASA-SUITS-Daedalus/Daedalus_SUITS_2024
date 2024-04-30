3a4d46345220524c364d4a96512461ae18440e43
Yining <106020817+ynzhang23@users.noreply.github.com>
2024-04-30 16:48:14 -0500

    [HMD-EGR] Create dynamic task status update and change backplate colors
    Completed up to EGR task 11.
    Reflect UIA and DCU switch status on egress task status, update text color based on the status of the switch.

--- HMD/Assets/Scripts/EGR/TaskController.cs
+++ HMD/Assets/Scripts/EGR/TaskController.cs
@@ -6,11 +6,17 @@ public class TaskController : MonoBehaviour
 {
     public TELEMETRYDataHandler telemetryDataHandler;
     public UIADataHandler uiaDataHandler;
+    public DCUDataHandler dcuDataHandler;
     public Transform background;
     public Transform foreground;
     public TextMeshPro stepTitleTextMeshPro;
     public TextMeshPro taskTextMeshPro;
     public TextMeshPro progressTextMeshPro;
+    public TextMeshPro taskStatusTextMeshPro;
+
+    public Color completedColor = Color.green;
+    public Color incompleteColor = Color.red;
+    public Color inProgressColor = Color.yellow;
 
     private string[] tasks = new string[]
     {
@@ -48,6 +54,12 @@ public class TaskController : MonoBehaviour
 
     private int currentTaskIndex = 0;
 
+    // Helper method to get the color name based on switch state
+    private string GetColorName(bool switchState)
+    {
+        return switchState ? "green" : "red";
+    }
+
     private void Start()
     {
         UpdateTaskText();
@@ -58,14 +70,88 @@ public class TaskController : MonoBehaviour
         UpdateProgressBar();
     }
 
+    private void ResetProgressBarAndText()
+    {
+        // Deactivate the progress bar
+        background.gameObject.SetActive(false);
+        foreground.gameObject.SetActive(false);
+        progressTextMeshPro.gameObject.SetActive(false);
+
+        // Reset the progress text
+        progressTextMeshPro.text = string.Empty;
+
+        // Deactivate the status text
+        taskStatusTextMeshPro.gameObject.SetActive(false);
+
+        // Reset the status text
+        taskStatusTextMeshPro.text = string.Empty;
+    }
+
     private void UpdateProgressBar()
     {
         int currentTaskIndex = TaskManager.Instance.GetCurrentTaskIndex();
-        float progress;
+        float progress = 0f;
 
         switch (currentTaskIndex)
         {
+            case 1: // Step 2: Power ON and Configure DCU
+                    // Reset progress bar and text
+                ResetProgressBarAndText();
+
+                // Check if EV-1 and EV-2 PWR switch is ON and BATT switch is UMB
+                bool isEVA1PowerOn = uiaDataHandler.GetPower("eva1");
+                bool isEVA2PowerOn = uiaDataHandler.GetPower("eva2");
+                bool isDCU1BattUMB = dcuDataHandler.GetBatt("eva1");
+                bool isDCU2BattUMB = dcuDataHandler.GetBatt("eva2");
+
+                if (isEVA1PowerOn && isEVA2PowerOn && isDCU1BattUMB && isDCU2BattUMB)
+                {
+                    progress = 1f;
+                }
+
+                // Update the status text with colored lines based on switch states
+                taskStatusTextMeshPro.gameObject.SetActive(true);
+                taskStatusTextMeshPro.text = $"<color={GetColorName(isEVA1PowerOn)}>EV-1 PWR: {(isEVA1PowerOn ? "ON" : "OFF")}</color>\n<color={GetColorName(isEVA2PowerOn)}>EV-2 PWR: {(isEVA2PowerOn ? "ON" : "OFF")}</color>\n<color={GetColorName(isDCU1BattUMB)}>DCU1 BATT: {(isDCU1BattUMB ? "ON" : "OFF")}</color>\n<color={GetColorName(isDCU2BattUMB)}>DCU2 BATT: {(isDCU2BattUMB ? "ON" : "OFF")}</color>";
+                break;
+
+            case 2: // Step 3: Start Depress
+                    // Reset progress bar and text
+                ResetProgressBarAndText();
+
+                // Check if DEPRESS PUMP PWR switch is ON
+                bool isDepressPumpPwrOn = uiaDataHandler.GetDepress();
+
+                if (isDepressPumpPwrOn)
+                {
+                    progress = 1f;
+                }
+
+                // Update the status text
+                taskStatusTextMeshPro.gameObject.SetActive(true);
+                taskStatusTextMeshPro.text = $"DEPRESS PUMP PWR: {(isDepressPumpPwrOn ? "ON" : "OFF")}";
+                break;
+
+            case 3: // Step 4: Prepare O2 Tanks - UIA: OXYGEN O2 VENT switch to OPEN
+                    // Reset progress bar and text
+                ResetProgressBarAndText();
+
+                // Check if OXYGEN O2 VENT switch is OPEN
+                bool isOxygenO2VentOpen = uiaDataHandler.GetOxy_Vent();
+
+                if (isOxygenO2VentOpen)
+                {
+                    progress = 1f;
+                }
+
+                // Update the status text
+                taskStatusTextMeshPro.gameObject.SetActive(true);
+                taskStatusTextMeshPro.text = $"OXYGEN O2 VENT: {(isOxygenO2VentOpen ? "OPEN" : "CLOSED")}";
+                break;
+
             case 4: // Step 4: Prepare O2 Tanks - Wait until Primary and Secondary OXY tanks < 10psi
+                    // Reset progress bar and text
+                ResetProgressBarAndText();
+
                 // Activate the progress bar
                 background.gameObject.SetActive(true);
                 foreground.gameObject.SetActive(true);
@@ -94,24 +180,133 @@ public class TaskController : MonoBehaviour
                 {
                     // GoForward();
                 }
-            break;
+                break;
+
+            case 5: // Step 4: Prepare O2 Tanks - UIA: OXYGEN O2 VENT switch to CLOSE
+                    // Reset progress bar and text
+                ResetProgressBarAndText();
+
+                // Check if OXYGEN O2 VENT switch is CLOSE
+                bool isOxygenO2VentClose = !uiaDataHandler.GetOxy_Vent();
+
+                if (isOxygenO2VentClose)
+                {
+                    progress = 1f;
+                }
+
+                // Update the status text
+                taskStatusTextMeshPro.gameObject.SetActive(true);
+                taskStatusTextMeshPro.text = $"OXYGEN O2 VENT: {(isOxygenO2VentClose ? "CLOSED" : "OPEN")}";
+                break;
+
+            case 6: // Step 4: Prepare O2 Tanks - Both DCUs: OXY switch to PRI
+                    // Reset progress bar and text
+                ResetProgressBarAndText();
+
+                // Check if OXY switch is PRI on both DCUs
+                bool isDCU1OxyPRI = dcuDataHandler.GetOxy("eva1");
+                bool isDCU2OxyPRI = dcuDataHandler.GetOxy("eva2");
+
+                if (isDCU1OxyPRI && isDCU2OxyPRI)
+                {
+                    progress = 1f;
+                }
+
+                // Update the status text
+                taskStatusTextMeshPro.gameObject.SetActive(true);
+                taskStatusTextMeshPro.text = $"<color={GetColorName(isDCU1OxyPRI)}>DCU1 OXY: {(isDCU1OxyPRI ? "PRI" : "SEC")}</color>\n<color={GetColorName(isDCU2OxyPRI)}>DCU2 OXY: {(isDCU2OxyPRI ? "PRI" : "SEC")}</color>";
+                break;
+
+            case 7: // Step 4: Prepare O2 Tanks - UIA: OXYGEN EMU-1 and EMU-2 switches to OPEN
+                    // Reset progress bar and text
+                ResetProgressBarAndText();
+
+                // Check if OXYGEN EMU-1 and EMU-2 switches are OPEN
+                bool isOxygenEMU1Open = uiaDataHandler.GetOxy("eva1");
+                bool isOxygenEMU2Open = uiaDataHandler.GetOxy("eva2");
+
+                if (isOxygenEMU1Open && isOxygenEMU2Open)
+                {
+                    progress = 1f;
+                }
+
+                // Update the status text
+                taskStatusTextMeshPro.gameObject.SetActive(true);
+                taskStatusTextMeshPro.text = $"<color={GetColorName(isOxygenEMU1Open)}>OXYGEN EMU-1: {(isOxygenEMU1Open ? "OPEN" : "CLOSED")}</color>\n<color={GetColorName(isOxygenEMU2Open)}>OXYGEN EMU-2: {(isOxygenEMU2Open ? "OPEN" : "CLOSED")}</color>";
+                break;
 
             case 8: // Step 4: Prepare O2 Tanks - Wait until EV1 and EV2 Primary O2 tanks > 3000 psi
-                // Activate the progress bar
+                    // Reset progress bar and text
+                ResetProgressBarAndText();
+
                 // Activate the progress bar
                 background.gameObject.SetActive(true);
                 foreground.gameObject.SetActive(true);
                 progressTextMeshPro.gameObject.SetActive(true);
 
-                primaryPressure = telemetryDataHandler.GetOxyPriPressure("eva1");
-                progress = Mathf.Clamp01(primaryPressure / 3000f);
+                float primaryPressureCase8 = telemetryDataHandler.GetOxyPriPressure("eva1");
+                progress = Mathf.Clamp01(primaryPressureCase8 / 3000f);
                 UpdateProgressBarSize(progress);
-                UpdateProgressText($"{primaryPressure:F0}psi (Current) / 3000psi (Goal)");
+                UpdateProgressText($"{primaryPressureCase8:F0}psi (Current) / 3000psi (Goal)");
 
-                if (primaryPressure > 3000f)
+                if (primaryPressureCase8 > 3000f)
                 {
                     // GoForward();
                 }
+                break;
+
+            case 9: // Step 4: Prepare O2 Tanks - UIA: OXYGEN EMU-1 and EMU-2 switches to CLOSE
+                    // Reset progress bar and text
+                ResetProgressBarAndText();
+
+                // Check if OXYGEN EMU-1 and EMU-2 switches are CLOSE
+                bool isOxygenEMU1Close = !uiaDataHandler.GetOxy("eva1");
+                bool isOxygenEMU2Close = !uiaDataHandler.GetOxy("eva2");
+
+                if (isOxygenEMU1Close && isOxygenEMU2Close)
+                {
+                    progress = 1f;
+                }
+
+                // Update the status text
+                taskStatusTextMeshPro.gameObject.SetActive(true);
+                taskStatusTextMeshPro.text = $"<color={GetColorName(isOxygenEMU1Close)}>OXYGEN EMU-1: {(isOxygenEMU1Close ? "CLOSED" : "OPEN")}</color>\n<color={GetColorName(isOxygenEMU2Close)}>OXYGEN EMU-2: {(isOxygenEMU2Close ? "CLOSED" : "OPEN")}</color>";
+                break;
+
+            case 10: // Step 4: Prepare O2 Tanks - Both DCUs: OXY switch to SEC
+                     // Reset progress bar and text
+                ResetProgressBarAndText();
+
+                // Check if OXY switch is SEC on both DCUs
+                bool isDCU1OxySEC = !dcuDataHandler.GetOxy("eva1");
+                bool isDCU2OxySEC = !dcuDataHandler.GetOxy("eva2");
+
+                if (isDCU1OxySEC && isDCU2OxySEC)
+                {
+                    progress = 1f;
+                }
+
+                // Update the status text
+                taskStatusTextMeshPro.gameObject.SetActive(true);
+                taskStatusTextMeshPro.text = $"<color={GetColorName(isDCU1OxySEC)}>DCU1 OXY: {(isDCU1OxySEC ? "SEC" : "PRI")}</color>\n<color={GetColorName(isDCU2OxySEC)}>DCU2 OXY: {(isDCU2OxySEC ? "SEC" : "PRI")}</color>";
+                break;
+
+            case 11: // Step 4: Prepare O2 Tanks - UIA: OXYGEN EMU-1 and EMU-2 switches to OPEN
+                     // Reset progress bar and text
+                ResetProgressBarAndText();
+
+                // Check if OXYGEN EMU-1 and EMU-2 switches are OPEN
+                bool isOxygenEMU1OpenCase11 = uiaDataHandler.GetOxy("eva1");
+                bool isOxygenEMU2OpenCase11 = uiaDataHandler.GetOxy("eva2");
+
+                if (isOxygenEMU1OpenCase11 && isOxygenEMU2OpenCase11)
+                {
+                    progress = 1f;
+                }
+
+                // Update the status text
+                taskStatusTextMeshPro.gameObject.SetActive(true);
+                taskStatusTextMeshPro.text = $"<color={GetColorName(isOxygenEMU1OpenCase11)}>OXYGEN EMU-1: {(isOxygenEMU1OpenCase11 ? "OPEN" : "CLOSED")}</color>\n<color={GetColorName(isOxygenEMU2OpenCase11)}>OXYGEN EMU-2: {(isOxygenEMU2OpenCase11 ? "OPEN" : "CLOSED")}</color>";
                 break;
 
             case 12: // Step 4: Prepare O2 Tanks - Wait until EV1 and EV2 Secondary O2 tanks > 3000 psi
@@ -186,12 +381,38 @@ public class TaskController : MonoBehaviour
                 }
                 break;
 
+
             default:
-                // Deactivate the progress bar for steps which require no progress bar
-                background.gameObject.SetActive(false);
-                foreground.gameObject.SetActive(false);
-                progressTextMeshPro.gameObject.SetActive(false);
+                // Reset progress bar and text for other cases
+                ResetProgressBarAndText();
                 break;
+        }
+
+        // Update the color of the progress bar based on the progress
+        if (progress < 1f)
+        {
+            if (progress > 0f)
+            {
+                foreground.GetComponent<Renderer>().material.color = inProgressColor;
+            }
+            else
+            {
+                foreground.GetComponent<Renderer>().material.color = incompleteColor;
+            }
+        }
+        else
+        {
+            foreground.GetComponent<Renderer>().material.color = completedColor;
+        }
+
+        // Update the task status text
+        if (progress < 1f)
+        {
+            taskStatusTextMeshPro.color = Color.red;
+        }
+        else
+        {
+            taskStatusTextMeshPro.color = Color.green;
         }
     }
 
