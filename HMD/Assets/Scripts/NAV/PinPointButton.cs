73db39ae2f6c7673108eb313fa5a40320699461a
BenRubin1 <rubinben28@gmail.com>
2024-05-04 04:06:14 -0500

    [HMD-NAV] Added Hazard pins with labeling. modified navigation hand menu to highlight active buttons

--- HMD/Assets/Scripts/NAV/PinPointButton.cs
+++ HMD/Assets/Scripts/NAV/PinPointButton.cs
@@ -1,109 +1,3 @@
-// using UnityEngine;
-// using UnityEngine.Events;
-// using Microsoft.MixedReality.Toolkit.Utilities;
-// using Microsoft.MixedReality.Toolkit.Input;
-
-// namespace Microsoft.MixedReality.Toolkit.UI
-// {
-//     public class PinPointButton : MonoBehaviour, IMixedRealitySpeechHandler
-//     {
-//         [SerializeField] private GameObject pinPointIconPrefab; // Drag your pin-point icon prefab here
-//         [SerializeField] private GameObject hazardPinPrefab; // Drag your hazard pin prefab here
-//         [SerializeField] private Transform spawnTransform; // Drag the GameObject you want to use as the spawn point here
-
-//         // Event to be invoked when the button is clicked
-//         public UnityEvent onGeneralPinClick = new UnityEvent();
-//         public UnityEvent onHazardPinClick = new UnityEvent();
-
-//         private bool isGeneralPinButtonPressed = false;
-
-//         private void Start()
-//         {
-//             // Register the speech command
-//             CoreServices.InputSystem?.RegisterHandler<IMixedRealitySpeechHandler>(this);
-//         }
-//         private void Update()
-//         {
-//             Debug.Log(isGeneralPinButtonPressed);
-//         }
-
-//         private void OnDestroy()
-//         {
-//             // Unregister the speech command
-//             CoreServices.InputSystem?.UnregisterHandler<IMixedRealitySpeechHandler>(this);
-//         }
-
-//         public void OnSpeechKeywordRecognized(SpeechEventData eventData)
-//         {
-//             if (isGeneralPinButtonPressed && eventData.Command.Keyword.ToLower() == "place pin")
-//             {
-//                 PlaceGeneralPin();
-//                 isGeneralPinButtonPressed = false; // Set to false after placing the pin
-//             }
-//         }
-
-//         public void OnGeneralPinButtonPressed()
-//         {
-//             isGeneralPinButtonPressed = true;
-//         }
-
-
-//         private void PlaceGeneralPin()
-//         {
-//             foreach (var source in MixedRealityToolkit.InputSystem.DetectedInputSources)
-//             {
-//                 if (source.SourceType == InputSourceType.Hand)
-//                 {
-//                     IMixedRealityPointer pointer = null;
-//                     foreach (var p in source.Pointers)
-//                     {
-//                         if (p is IMixedRealityNearPointer)
-//                         {
-//                             continue;
-//                         }
-//                         pointer = p;
-//                         break;
-//                     }
-
-//                     if (pointer != null && pointer.Result != null && pointer.Result.Details.Object != null)
-//                     {
-//                         var hitPoint = pointer.Result.Details.Point;
-
-//                         // Get the height of the headset
-//                         float headsetHeight = CameraCache.Main.transform.position.y;
-
-//                         // Set the pin height relative to the headset height
-//                         Vector3 pinPosition = new Vector3(hitPoint.x, headsetHeight - 0.5f, hitPoint.z);
-
-//                         // Check if the pin position is too close to an existing pin
-//                         if (!IsPinTooCloseToExistingPin(pinPosition))
-//                         {
-//                             GameObject pin = Instantiate(pinPointIconPrefab, pinPosition, Quaternion.identity);
-//                             // Perform any additional setup or configuration for the instantiated pin
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-
-//         private bool IsPinTooCloseToExistingPin(Vector3 pinPosition)
-//         {
-//             // Adjust the minimum distance as needed
-//             float minDistance = 0.1f;
-
-//             // Check the distance between the new pin position and existing pins
-//             foreach (GameObject existingPin in GameObject.FindGameObjectsWithTag("Pin"))
-//             {
-//                 if (Vector3.Distance(pinPosition, existingPin.transform.position) < minDistance)
-//                 {
-//                     return true;
-//                 }
-//             }
-//             return false;
-//         }
-//     }
-// }
-
 using UnityEngine;
 using UnityEngine.Events;
 using TMPro;
@@ -116,14 +10,22 @@ namespace Microsoft.MixedReality.Toolkit.UI
     {
         [SerializeField] private GameObject pinPointIconPrefab; // Drag your pin-point icon prefab here
         [SerializeField] private GameObject hazardPinPrefab; // Drag your hazard pin prefab here
+        [SerializeField] private GameObject samplePinPrefab; // Drag your sample pin prefab here
         [SerializeField] private Transform spawnTransform; // Drag the GameObject you want to use as the spawn point here
-
+        private int samplePinCount = 0;
+        private string labelText;
+        [SerializeField] private GameObject generalPinBackplate;
+        [SerializeField] private GameObject hazardPinBackplate;
+        [SerializeField] private GameObject samplePinBackplate;
         // Event to be invoked when the button is clicked
         public UnityEvent onGeneralPinClick = new UnityEvent();
         public UnityEvent onHazardPinClick = new UnityEvent();
+        public UnityEvent onSamplePinClick = new UnityEvent();
 
         private bool isGeneralPinButtonPressed = false;
         private bool isHazardPinButtonPressed = false;
+        private bool isSamplePinButtonPressed = false;
+        private string currentLabel = string.Empty;
         private const float VISIBILITY_DISTANCE = 50f; // Distance threshold for pin visibility
 
         private void Start()
@@ -131,10 +33,12 @@ namespace Microsoft.MixedReality.Toolkit.UI
             // Register the speech command
             CoreServices.InputSystem?.RegisterHandler<IMixedRealitySpeechHandler>(this);
         }
+
         private void Update()
         {
             Debug.Log(isGeneralPinButtonPressed);
             Debug.Log(isHazardPinButtonPressed);
+            Debug.Log(isSamplePinButtonPressed);
         }
 
         private void OnDestroy()
@@ -149,33 +53,76 @@ namespace Microsoft.MixedReality.Toolkit.UI
             {
                 if (isGeneralPinButtonPressed)
                 {
+                    labelText = "";
                     PlacePin(pinPointIconPrefab);
                     isGeneralPinButtonPressed = false; // Set to false after placing the pin
                 }
                 else if (isHazardPinButtonPressed)
                 {
+                    labelText = "";
                     PlacePin(hazardPinPrefab);
                     isHazardPinButtonPressed = false; // Set to false after placing the pin
+                }
+                else if (isSamplePinButtonPressed)
+                {
+                    samplePinCount++;
+                    labelText = $"Sample {samplePinCount}: ";
+                    PlacePin(samplePinPrefab);
+                    isSamplePinButtonPressed = false; // Set to false after placing the pin
                 }
             }
         }
 
         public void OnGeneralPinButtonPressed()
         {
-            if (!isHazardPinButtonPressed)
-            {
-                isGeneralPinButtonPressed = true;
-            }
+            isGeneralPinButtonPressed = true;
+            isHazardPinButtonPressed = false;
+            isSamplePinButtonPressed = false;
+            HighlightButton(generalPinBackplate);
+            UnhighlightButton(hazardPinBackplate);
+            UnhighlightButton(samplePinBackplate);
         }
 
         public void OnHazardPinButtonPressed()
         {
-            if (!isGeneralPinButtonPressed)
+            isGeneralPinButtonPressed = false;
+            isHazardPinButtonPressed = true;
+            isSamplePinButtonPressed = false;
+            HighlightButton(hazardPinBackplate);
+            UnhighlightButton(generalPinBackplate);
+            UnhighlightButton(samplePinBackplate);
+            
+        }
+
+        public void OnSamplePinButtonPressed()
+        {
+            isGeneralPinButtonPressed = false;
+            isHazardPinButtonPressed = false;
+            isSamplePinButtonPressed = true;
+            HighlightButton(samplePinBackplate);
+            UnhighlightButton(generalPinBackplate);
+            UnhighlightButton(hazardPinBackplate);
+            
+        
+        }
+
+        private void HighlightButton(GameObject backplate)
+        {
+            // Activate the transparent yellow backplate
+            if (backplate != null)
             {
-                isHazardPinButtonPressed = true;
+                backplate.SetActive(true);
             }
         }
 
+        private void UnhighlightButton(GameObject backplate)
+        {
+            // Deactivate the transparent yellow backplate
+            if (backplate != null)
+            {
+                backplate.SetActive(false);
+            }
+        }
 
         private void PlacePin(GameObject pinPrefab)
         {
@@ -210,17 +157,31 @@ namespace Microsoft.MixedReality.Toolkit.UI
                             GameObject pin = Instantiate(pinPrefab, pinPosition, Quaternion.identity);
                             // Perform any additional setup or configuration for the instantiated pin
                             // Get the TMP_Text component from the pin prefab
-                           // Get the TMP_Text component from the pin prefab
                             TMP_Text distanceText = pin.GetComponentInChildren<TMP_Text>();
-
+                            if (pinPrefab == pinPointIconPrefab)
+                            {
+                                UnhighlightButton(generalPinBackplate);
+                            }
+                            if (pinPrefab == hazardPinPrefab)
+                            {
+                                UnhighlightButton(hazardPinBackplate);
+                            }
+                            if (pinPrefab == samplePinPrefab)
+                            {
+                                UnhighlightButton(samplePinBackplate);
+                            }
                             // Start a coroutine to update the distance and rotation continuously
-                            StartCoroutine(UpdatePinState(pin.transform, distanceText));
+                            StartCoroutine(UpdatePinState(pin.transform, distanceText, labelText));
                         }
                     }
                 }
             }
         }
-        private System.Collections.IEnumerator UpdatePinState(Transform pinTransform, TMP_Text distanceText)
+
+        
+
+
+        private System.Collections.IEnumerator UpdatePinState(Transform pinTransform, TMP_Text distanceText, string pinlabelText)
         {
             Renderer pinRenderer = pinTransform.GetComponentInChildren<Renderer>();
 
@@ -229,8 +190,15 @@ namespace Microsoft.MixedReality.Toolkit.UI
                 // Calculate the distance between the pin and the headset
                 float distance = Vector3.Distance(pinTransform.position, CameraCache.Main.transform.position);
 
-                // Update the text to display the distance
-                distanceText.text = $"{distance:F2}m";
+                // Update the text to display the label and distance
+                if (!string.IsNullOrEmpty(pinlabelText))
+                {
+                    distanceText.text = $"{pinlabelText}  {distance:F2}m";
+                }
+                else
+                {
+                    distanceText.text = $"{distance:F2}m";
+                }
 
                 // Rotate the TMP object to face the camera
                 distanceText.transform.rotation = Quaternion.LookRotation(distanceText.transform.position - CameraCache.Main.transform.position);
@@ -245,7 +213,7 @@ namespace Microsoft.MixedReality.Toolkit.UI
         private bool IsPinTooCloseToExistingPin(Vector3 pinPosition)
         {
             // Adjust the minimum distance as needed
-            float minDistance = 0.1f;
+            float minDistance = 0f;
 
             // Check the distance between the new pin position and existing pins
             foreach (GameObject existingPin in GameObject.FindGameObjectsWithTag("Pin"))
